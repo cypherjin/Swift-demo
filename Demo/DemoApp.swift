887be59b07b4843ffe15fbfbcbc0ac328a11b2a4
Shubham Jindal <shubhamjindal@outlook.in>
2024-06-13 21:28:02 +0530

    Initial Commit

---
+++ Demo/DemoApp.swift
@@ -0,0 +1,32 @@
+//
+//  DemoApp.swift
+//  Demo
+//
+//  Created by Shubham Jindal on 13/06/24.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct DemoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
